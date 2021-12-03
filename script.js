a55a08429f1ce6eb966fa69bb6e33ddd7dd92b5d
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-03 12:20:14 -0300

    function verificar()

---
+++ script.js
@@ -0,0 +1,13 @@
+function verificar() {
+    var data = new Date()
+    var ano  = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano ) {
+        alert("*[ERRO]* Verifique os dados e tente novamente!")
+    } else {
+       var fsex =  document.getElementsByTagName('radsex')
+       var idade = ano - Number(fano.value)
+       
+    }
+}

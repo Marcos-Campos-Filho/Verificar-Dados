322f1543b46c913de9571edef0ac0b6f536d4148
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-03 13:47:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,36 @@ function verificar() {
        img.setAttribute ('img','foto')
        if (radius[0].checked) {
           gênero = 'Homem'
+          if (idade >= 0 && idade < 10) {
+               //CRIANÇA
+               img.setAttribute('src', 'midia/foto-criança-h-250.png')
+          } else if (  idade <= 21 ) {
+              //JOVEM
+              img.setAttribute('src', 'midia/foto-jovem-h-250.png')
+          }else if (idade <= 50) {
+              //ADULTO
+              img.setAttribute('src', 'midia/foto-adulto-h-250.png')
+              
+          }else {
+              //IDOSO
+              img.setAttribute('src', 'midia/foto-idoso-h-250.png')
+          }
        }else if (radius[1].checked) {
           gênero = 'Mulher'
+          if (idade >= 0 && idade < 10) {
+            //CRIANÇA
+       } else if (idade >= 10 && idade < 21 ) {
+           //JOVEM
+       }else if (idade >= 21 && idade < 50) {
+           //ADULTO
+       }else {
+           //IDOSO
+       }
        }else {
            gênero = 'uma pessoa Não-Binâria'
        } 
        res.style.textAlign = 'center'
        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+       res.appendChild(img)
     }
 }

58cb664de12d4bd4673a0640375b7142f6833e31
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-03 13:19:23 -0300

    add nb media / uptade script.js

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano ) {
         alert("*[ERRO]* Verifique os dados e tente novamente!")
     } else {
-       var fsex =  document.getElementsByTagName('radsex')
+       var radius = document.querySelectorAll("input[type='radio']")
        var idade = ano - Number(fano.value)
-       
+       var gênero = ''
+       var img = document.createElement('img')
+       img.setAttribute ('img','foto')
+       if (radius[0].checked) {
+          gênero = 'Homem'
+       }else if (radius[1].checked) {
+          gênero = 'Mulher'
+       }else {
+           gênero = 'uma pessoa Não-Binâria'
+       } 
+       res.style.textAlign = 'center'
+       res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 }

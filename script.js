afb2e322dfa94f4cb5964a703a20d851304ed93f
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-03 14:08:59 -0300

    Final

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function verificar() {
        img.setAttribute ('img','foto')
        if (radius[0].checked) {
           gênero = 'Homem'
-          if (idade >= 0 && idade < 10) {
+          if (idade >= 0 && idade <= 10) {
                //CRIANÇA
                img.setAttribute('src', 'midia/foto-criança-h-250.png')
           } else if (  idade <= 21 ) {
@@ -28,13 +28,13 @@ function verificar() {
           }
        }else if (radius[1].checked) {
           gênero = 'Mulher'
-          if (idade >= 0 && idade < 10) {
+          if (idade >= 0 && idade <= 10) {
             //CRIANÇA
             img.setAttribute('src', 'midia/foto-criança-m-250.png')
-       } else if (idade >= 10 && idade < 21 ) {
+       } else if ( idade <= 21 ) {
            //JOVEM
            img.setAttribute('src', 'midia/foto-jovem-m-250.png')
-       }else if (idade >= 21 && idade < 50) {
+       }else if ( idade <= 50) {
            //ADULTO
            img.setAttribute('src', 'midia/foto-adulto-m-250.png')
        }else {
@@ -43,6 +43,20 @@ function verificar() {
        }
        }else {
            gênero = 'uma pessoa Não-Binâria'
+           if (idade >= 0 && idade <= 10) {
+            //CRIANÇA
+            img.setAttribute('src', 'midia/foto-criança-nb-250.png')
+       } else if ( idade <= 21 ) {
+           //JOVEM
+           img.setAttribute('src', 'midia/foto-jovem-nb-250.png')
+       }else if ( idade <= 50) {
+           //ADULTO
+           img.setAttribute('src', 'midia/foto-adulto-nb-250.png')
+       }else {
+           //IDOSO
+           img.setAttribute('src', 'midia/foto-idoso-nb-250.png')
+       }
+
        } 
        res.style.textAlign = 'center'
        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
